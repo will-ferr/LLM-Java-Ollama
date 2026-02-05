b2327fafea74bb9586f479f2edac655114b84a79
Will-ferr <william_ferreira00@outlook.com>
2026-02-05 13:08:09 -0300

    Atualização do projeto

---
+++ src/main/java/LLM_API/OLLM/OllmApplication.java
@@ -0,0 +1,13 @@
+package LLM_API.OLLM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LLM_API/OLLM/OllmApplicationTests.java
@@ -0,0 +1,13 @@
+package LLM_API.OLLM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
